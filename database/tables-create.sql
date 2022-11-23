14522e41f1bc30cdc2afb76dec868efee22066cf
Cathy Sarisky <catsarisky@gmail.com>
2022-11-23 13:55:34 -0500

    updates and comments on sql

--- database/tables-create.sql
+++ database/tables-create.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users(
+CREATE TABLE users (
   user_id int NOT NULL,
   user_first_name char(64) NOT NULL,
   user_last_name char(64) NOT NULL,
@@ -15,32 +15,34 @@ CREATE TABLE posts(
   post_id int NOT NULL,
   user_id int NOT NULL,
   post_title char(34),
-  post_content varchar(65500),
-  post_image varchar(65500),
+  post_content varchar(30000),
+  -- I needed to make these smaller to load the table.  They were bigger than the database can 
+  -- handle (badly documented issue, apparently), and really, posts don't need to be dozens of pages long!
+  post_image varchar(1000),
   post_likes_score int,
   post_comments_count int NOT NULL,
   post_visible bit NOT NULL, 
-  comment_id int NOT NULL,
-  commenter_id int NOT NULL,
-  comment_content varchar(65500),
-  comment_timestamp DATETIME NOT NULL, 
   PRIMARY KEY(post_id),
   UNIQUE(post_id),
   FOREIGN KEY (user_id) REFERENCES users(user_id)  
 );
 
 CREATE TABLE group_memberships(
+  /* please recheck against the database design - you have stuff in here that doesn't belong */
   group_id int NOT NULL,
-  user_id int NOT NULL
+  user_id int NOT NULL,
   owner_id int NOT NULL,
-  group_name char(34) NOT NULL,
+  group_name varchar(34) NOT NULL,
   group_ranking bit NOT NULL,
   PRIMARY KEY(group_id),
   UNIQUE(group_name),
   FOREIGN KEY (user_id) REFERENCES users(user_id)  
 );
 
-CREATE TABLE post_groups(
+/* CREATE TABLE post_groups(
+
+What's up here?  post_groups is supposed to have only post_id and group_id.
+
   group_id int NOT NULL,
   user_id int NOT NULL
   post_id int NOT NULL,
@@ -50,7 +52,7 @@ CREATE TABLE post_groups(
   group_ranking int NOT NULL,
   post_title char(64),
   post_content varchar(65500),
-  post_image varchar(655000), 
+  post_image varchar(65500), 
   comment_id int NOT NULL,
   commenter_id int NOT NULL,
   comment_content varchar(65500),
@@ -59,36 +61,41 @@ CREATE TABLE post_groups(
   post_weight int,
   FOREIGN KEY (user_id) REFERENCES users(user_id)  
 );  
+*/ 
   
 CREATE TABLE friendships(
+  user_id int NOT NULL,
   friend_id int NOT NULL,
   friendship_status enum('nominal', 'accepted', 'pending', 'blocked', 'unblocked') NOT NULL,
   friend_request DATE NOT NULL, 
-  PRIMARY KEY (friend_id),
+  PRIMARY KEY (user_id, friend_id),
   UNIQUE (friend_id), 
   FOREIGN KEY (user_id) REFERENCES users(user_id)  
 );
 
 CREATE TABLE reactions(
-  reaction_id int,
+ /* I rewrote a little bit - didn't match the database design */ 
   user_id int NOT NULL,
-  post_weight int,
-  PRIMARY KEY(reaction_id),
-  UNIQUE(reaction_id),
-  FOREIGN KEY (user_id) REFERENCES users(user_id)  
+  post_id int,
+  PRIMARY KEY(user_id, post_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  FOREIGN KEY (post_id) REFERENCES posts(post_id)
   );
 
 CREATE TABLE comments(
   comment_id int NOT NULL,
   commenter_id int NOT NULL,
-  user_id int NOT NULL,
-  comment_content varchar(65500),
+  --user_id int NOT NULL,
+  --user_id and commenter_id are the same. Pick one. :)
+  comment_content varchar(10000),
   comment_timestamp DATETIME NOT NULL,
-  reaction_id int,
-  post_weight int,
+  /* can't store reaction_id here - it's not a 1:1 mapping */ 
+  -- reaction_id int,
+  post_id int,
   PRIMARY_KEY(comment_id),
   UNIQUE(comment_id),
-  FOREIGN KEY (user_id) REFERENCES users(user_id)  
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  FOREIGN KEY (post_id) REFERENCES posts(post_id)
 );
 
 CREATE TABLE sessions(
@@ -101,6 +108,10 @@ CREATE TABLE sessions(
 
 
 CREATE TABLE posts_feed(
+
+  /* this one needs some more attention. The goal isn't to store the whole feed in one table, but just to store the 
+  user_id, post_id, and post_weight.  See the database design document. 
+  */  
   user_id int NOT NULL, 
   post_id int NOT NULL,
   session_id varchar(255) NOT NULL,
@@ -119,4 +130,4 @@ CREATE TABLE posts_feed(
   FOREIGN KEY (user_id) REFERENCES users(user_id)  
 );
 
-
+/* Missing a table - groups, I think? */
