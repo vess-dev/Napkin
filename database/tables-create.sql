4b31d81bc990d4f89768432e9488a08cd2518345
TaylorLewis30 <117309326+TaylorLewis30@users.noreply.github.com>
2022-11-23 15:58:10 +0000

    Update tables-create.sql

--- database/tables-create.sql
+++ database/tables-create.sql
@@ -1,103 +1,123 @@
-create table users(
-user_id char(32),
-user_first_name char(64),
-user_last_name char(64),
-user_email char(64),
-user_password char(32),
-user_handle char(32),
-user_status char(16),
-primary key(user_id)
+CREATE TABLE users(
+  user_id int NOT NULL,
+  user_first_name char(64) NOT NULL,
+  user_last_name char(64) NOT NULL,
+  user_email char(64) NOT NULL,
+  user_password char(32) NOT NULL,
+  user_handle char(32) NOT NULL,
+  user_status char(16) NOT NULL,
+  PRIMARY KEY (user_id)
+  UNIQUE (user_handle)
 );
 
 
 
-create table posts(
-post_id char(32),
-post_title char(34),
-post_content varchar(0 to 65500),
-post_image varchar(0 to 65500),
-post_likes_score int(-2147483648 to 2147483648),
-post_comments_count int(-2147483648 to 2147483648),
-post_visible bool(0 false, any non zero int is true), 
-primary key(post_id)
-comment_id char(32),
-commenter_id char(32),
-comment_content varchar(0 to 65500),
-comment_timestamp timestamp(1985-09-25 17:45:30.005),
+CREATE TABLE posts(
+  post_id int NOT NULL,
+  user_id int NOT NULL,
+  post_title char(34),
+  post_content varchar(65500),
+  post_image varchar(65500),
+  post_likes_score int,
+  post_comments_count int NOT NULL,
+  post_visible bit NOT NULL, 
+  comment_id int NOT NULL,
+  commenter_id int NOT NULL,
+  comment_content varchar(65500),
+  comment_timestamp DATETIME NOT NULL, 
+  PRIMARY KEY(post_id),
+  UNIQUE(post_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id)  
 );
 
-create table group_memberships(
-group_id char(32),
-owner_id char(32),
-group_name char(34),
-group_ranking int(1 to 5),
-primary key(group_id)
+CREATE TABLE group_memberships(
+  group_id int NOT NULL,
+  user_id int NOT NULL
+  owner_id int NOT NULL,
+  group_name char(34) NOT NULL,
+  group_ranking bit NOT NULL,
+  PRIMARY KEY(group_id),
+  UNIQUE(group_name),
+  FOREIGN KEY (user_id) REFERENCES users(user_id)  
 );
 
-create table post_groups(
-group_id char(32),
-post_id char(32),
-poster_id char(32)
-owner_id char(32),
-group_name char(34),
-group_ranking int(1 to 5),
-post_title char(64),
-post_content varchar(0 to 65500),
-post_image varchar(0 to 655000), 
-comment_id char(32),
-commenter_id char(32),
-comment_content varchar(0 to 65500),
-comment_timestamp timestamp(1985-09-25 17:45:30.005),
-reaction_id int(0 to 1),
-post_weight int(0 to MAX),
-primary key (group_id)
+CREATE TABLE post_groups(
+  group_id int NOT NULL,
+  user_id int NOT NULL
+  post_id int NOT NULL,
+  poster_id int NOT NULL,
+  owner_id int NOT NULL,
+  group_name char(34) NOT NULL,
+  group_ranking int NOT NULL,
+  post_title char(64),
+  post_content varchar(65500),
+  post_image varchar(655000), 
+  comment_id int NOT NULL,
+  commenter_id int NOT NULL,
+  comment_content varchar(65500),
+  comment_timestamp DATETIME NOT NULL,
+  reaction_id int,
+  post_weight int,
+  FOREIGN KEY (user_id) REFERENCES users(user_id)  
 );  
   
-create table friendships(
-friend_id char(32),
-friendship_status enum('nominal', 'accepted', 'pending', 'blocked', 'unblocked'),
-friend_request_timestamp(1985-09-25 17:45:30.005),
-primary key(friend_id)
+CREATE TABLE friendships(
+  friend_id int NOT NULL,
+  friendship_status enum('nominal', 'accepted', 'pending', 'blocked', 'unblocked') NOT NULL,
+  friend_request DATE NOT NULL, 
+  PRIMARY KEY (friend_id),
+  UNIQUE (friend_id), 
+  FOREIGN KEY (user_id) REFERENCES users(user_id)  
 );
 
-create table reactions(
-reaction_id int(0 to 1),
-post_weight int(0 to MAX),
-primary key(reaction_id)
+CREATE TABLE reactions(
+  reaction_id int,
+  user_id int NOT NULL,
+  post_weight int,
+  PRIMARY KEY(reaction_id),
+  UNIQUE(reaction_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id)  
+  );
+
+CREATE TABLE comments(
+  comment_id int NOT NULL,
+  commenter_id int NOT NULL,
+  user_id int NOT NULL,
+  comment_content varchar(65500),
+  comment_timestamp DATETIME NOT NULL,
+  reaction_id int,
+  post_weight int,
+  PRIMARY_KEY(comment_id),
+  UNIQUE(comment_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id)  
 );
 
-create table comments(
-comment_id char(32),
-commenter_id char(32),
-comment_content varchar(0 to 65500),
-comment_timestamp timestamp(1985-09-25 17:45:30.005),
-reaction_id int(0 to 1),
-post_weight int(0 to MAX),
-primary_key(comment_id)
-);
-
-create table sessions(
-session_id char(32),
-primary key(session_id)
+CREATE TABLE sessions(
+  user_id int NOT NULL,
+  session_id varchar(255) NOT NULL,
+  PRIMARY KEY(session_id),
+  UNIQUE(session_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id)           
 );
 
 
-create table posts_feed(
-post_id char(32),
-session_id char(32),
-comment_id char(32),
-commenter_id char(32),
-comment_content varchar(0 to 65500),
-comment_timestamp timestamp(1985-09-25 17:45:30.005),
-post_title char(34),
-post_content varchar(0 to 65500),
-post_image varchar(0 to 65500),
-post_likes_score int(-2147483648 to 2147483648),
-post_comments_count int(-2147483648 to 2147483648),
-post_visible bool(0 false, any non zero int is true),
-reaction_id int(0 to 1),
-post_weight int(0 to MAX),
-primary key(post_id)
+CREATE TABLE posts_feed(
+  user_id int NOT NULL, 
+  post_id int NOT NULL,
+  session_id varchar(255) NOT NULL,
+  comment_id int NOT NULL,
+  commenter_id int NOT NULL,
+  comment_content varchar(65500),
+  comment_timestamp DATETIME NOT NULL,
+  post_title char(34),
+  post_content varchar(65500) NOT NULL,
+  post_image varchar(65500) NOT NULL,
+  post_likes_score int,
+  post_comments_count int,
+  post_visible bit,
+  reaction_id int,
+  post_weight int,
+  FOREIGN KEY (user_id) REFERENCES users(user_id)  
 );
 
 
