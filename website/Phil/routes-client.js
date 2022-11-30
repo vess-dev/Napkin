cdbb71a2ded6bfa1747c7932892eed93427baedd
Vess <vess.contact@protonmail.com>
2022-11-30 09:36:42 -0500

    More Napkiny

--- website/Phil/routes-client.js
+++ website/Phil/routes-client.js
@@ -1,9 +1,9 @@
-const SERVER = 'http://cpsc.roanoke.edu:3009/';
+const SERVER = "http://cpsc.roanoke.edu:3009/";
 
-const LOGIN_PAGE = '#login';
-const HOME_PAGE = '#home';
-const ADD_USER_PAGE = '#addUser';
-const LIST_USERS_PAGE = '#listUsers';
-const GET_POSTS_PAGE = '#getPosts' ;
+const LOGIN_PAGE = "#login";
+const HOME_PAGE = "#home";
+const ADD_USER_PAGE = "#addUser";
+const LIST_USERS_PAGE = "#listUsers";
+const GET_POSTS_PAGE = "#getPosts" ;
 
 export {SERVER, LOGIN_PAGE, HOME_PAGE, ADD_USER_PAGE, LIST_USERS_PAGE, GET_POSTS_PAGE};
