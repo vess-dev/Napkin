c88b4d07d59f219ee1ac2d27b429e6435849c37b
Cathy Sarisky <catsarisky@gmail.com>
2022-11-29 19:35:50 -0500

    failure to count to three.

--- server/user.js
+++ server/user.js
@@ -26,8 +26,8 @@ function getUserList() {
 function searchUsers(searchstring) {
   console.log('got into searchUsers and searchstring is', searchstring)
   return new Promise((resolve, reject) =>{
-    db.pool.query('select user_handle, users.user_id, user_image, user_first_name from users WHERE user_first_name LIKE ? OR user_last_name LIKE ? OR user_handle LIKE ?', 
-      [`'%`+searchstring+`%'`,`'%`+searchstring+`%'`],
+    db.pool.query('select user_handle, users.user_id, user_image, user_first_name from users WHERE (user_first_name LIKE ?) OR (user_last_name LIKE ?) OR (user_handle LIKE ?)', 
+      [`'%`+searchstring+`%'`,`'%`+searchstring+`%'`,`'%`+searchstring+`%'`],
       function(error, results) {
         console.log(results)
         if (error) {                    
