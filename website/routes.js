9258132dd3060337a41239bcb65c0c4a7d17f88a
Vess <vess.contact@protonmail.com>
2022-11-30 11:00:13 -0500

    testing - broken

--- website/routes.js
+++ website/routes.js
@@ -1,12 +1,16 @@
 const SERVER = "http://cpsc.roanoke.edu:3009/";
 
 // User Paths
-const PAGE_LOGIN = "#login";
+const PAGE_ACC_LOGIN = "#accLogin";
 const PAGE_ACC_CREATE = "#accCreate";
 const PAGE_ACC_PENDING = "#accPending";
 const PAGE_MY_FEED = "#myFeed"; // myFeed = Friends
 const PAGE_ACC_FEED = "#accFeed"; // accFeed = My Account
-const PAGE_ACC_FRIENDS = "accFriends"
+const PAGE_ACC_FRIENDS = "#accFriends";
+const PAGE_ACC_BLOCKED = "#accBlocked";
+const PAGE_SEARCH = "#friendSearch";
+const PAGE_ACC_GROUPS = "#accGroups";
+const PAGE_POST_CREATE = "#postCreate";
 
 // Admin Paths
 
