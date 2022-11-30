14aff4c955be778bebe0b5e7bfce5d9ca9869f67
Vess <vess.contact@protonmail.com>
2022-11-30 08:59:18 -0500

    Cleanup

---
+++ website/Phil/ClientError.js
@@ -0,0 +1,9 @@
+export class ClientError extends Error {
+	constructor (name, statusCode, description) {
+		super(description)
+		Object.setPrototypeOf(this, new.target.prototype)
+		this.name = name
+		this.statusCode = statusCode
+		Error.captureStackTrace(this)
+	}
+}
