7d6d09d462a1f24b8ff1f039decb17fb5d18c130
Vess <vess.contact@protonmail.com>
2022-11-30 10:45:03 -0500

    temp paths

---
+++ website/routes.js
@@ -0,0 +1,15 @@
+const SERVER = "http://cpsc.roanoke.edu:3009/";
+
+// User Paths
+const PAGE_LOGIN = "#login";
+const PAGE_ACC_CREATE = "#accCreate";
+const PAGE_ACC_PENDING = "#accPending";
+const PAGE_MY_FEED = "#myFeed"; // myFeed = Friends
+const PAGE_ACC_FEED = "#accFeed"; // accFeed = My Account
+const PAGE_ACC_FRIENDS = "accFriends"
+
+// Admin Paths
+
+//"#listUsers";
+
+export {SERVER};
