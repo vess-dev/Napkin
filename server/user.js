da9f80e73ed49b7e5f5b7f9b43597c78893f2013
Cathy Sarisky <catsarisky@gmail.com>
2022-11-29 19:34:05 -0500

    add extra logging

--- server/user.js
+++ server/user.js
@@ -24,6 +24,7 @@ function getUserList() {
 }
 
 function searchUsers(searchstring) {
+  console.log('got into searchUsers and searchstring is', searchstring)
   return new Promise((resolve, reject) =>{
     db.pool.query('select user_handle, users.user_id, user_image, user_first_name from users WHERE user_first_name LIKE ? OR user_last_name LIKE ? OR user_handle LIKE ?', 
       [`'%`+searchstring+`%'`,`'%`+searchstring+`%'`],
