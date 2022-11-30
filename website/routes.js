8173e71fd9a7f560abc58eb1444e24f36e2e6883
Vess <vess.contact@protonmail.com>
2022-11-30 11:24:33 -0500

    Update routes.js

--- website/routes.js
+++ website/routes.js
@@ -1,19 +1,21 @@
-const SERVER = "http://cpsc.roanoke.edu:3009/";
+export const SERVER = "http://cpsc.roanoke.edu:3009/";
 
 // User Paths
-const PAGE_ACC_LOGIN = "#accLogin";
-const PAGE_ACC_CREATE = "#accCreate";
-const PAGE_ACC_PENDING = "#accPending";
-const PAGE_MY_FEED = "#myFeed"; // myFeed = Friends
-const PAGE_ACC_FEED = "#accFeed"; // accFeed = My Account
-const PAGE_ACC_FRIENDS = "#accFriends";
-const PAGE_ACC_BLOCKED = "#accBlocked";
-const PAGE_SEARCH = "#friendSearch";
-const PAGE_ACC_GROUPS = "#accGroups";
-const PAGE_POST_CREATE = "#postCreate";
+export const PAGE_ACC_LOGIN = "#accLogin";
+export const PAGE_ACC_CREATE = "#accCreate";
+export const PAGE_ACC_PENDING = "#accPending";
+export const PAGE_MY_FEED = "#myFeed"; // myFeed = Friends
+export const PAGE_ACC_FEED = "#accFeed"; // accFeed = My Account
+export const PAGE_ACC_FRIENDS = "#accFriends";
+export const PAGE_ACC_BLOCKED = "#accBlocked";
+export const PAGE_SEARCH = "#friendSearch";
+export const PAGE_ACC_GROUPS = "#accGroups";
+export const PAGE_POST_CREATE = "#postCreate";
+export const PAGE_POST_EDIT = "#postEdit";
+export const PAGE_ACC_SETTINGS = "#accSettings";
 
 // Admin Paths
-
-//"#listUsers";
-
-export {SERVER};
+export const PAGE_ADMIN_PENDING = "#adminPending";
+export const PAGE_ADMIN_DISABLED = "#adminDisabled";
+export const PAGE_ADMIN_ACTIVE = "#adminActive";
+export const PAGE_ADMIN_PENDING = "#adminBlacklist";
