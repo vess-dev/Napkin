c23a4697b727804357040c66c41c6fba8d03c2ea
Vess <vess.contact@protonmail.com>
2022-12-02 22:20:48 -0500

    fix routing

--- website/script.js
+++ website/script.js
@@ -29,35 +29,54 @@ function routePage(newPage) {
 	switch(currentHash) {
 		case routes.PAGE_ACC_LOGIN:
 			pages.accLogin();
+			break;
 		case routes.PAGE_ACC_CREATE:
 			pages.accCreate();
+			break;
 		case routes.PAGE_ACC_PENDING:
 			pages.accPending();
+			break;
 		case routes.PAGE_MY_FEED:
 			pages.myFeed();
+			break;
 		case routes.PAGE_ACC_FEED:
 			pages.accFeed();
+			break;
 		case routes.PAGE_ACC_FRIENDS:
 			pages.accFriends();
+			break;
 		case routes.PAGE_ACC_BLOCKED:
 			pages.accBlocked();
+			break;
 		case routes.PAGE_SEARCH:
 			pages.friendSearch();
+			break;
 		case routes.PAGE_ACC_GROUPS:
 			pages.accGroups();
+			break;
 		case routes.PAGE_POST_CREATE:
 			pages.postCreate();
+			break;
 		case routes.PAGE_POST_EDIT:
 			pages.postEdit();
+			break;
 		case routes.PAGE_ACC_SETTINGS:
 			pages.accSettings();
+			break;
 		case routes.PAGE_ADMIN_PENDING:
 			pages.adminPending();
+			break;
 		case routes.PAGE_ADMIN_DISABLED:
 			pages.adminDisabled();
+			break;
 		case routes.PAGE_ADMIN_ACTIVE:
 			pages.adminActive();
+			break;
 		case routes.PAGE_ADMIN_BLACKLIST:
 			pages.adminBlacklist();
+			break;
+		default:
+			console.log("Unhandled route.");
+			break;
 	}	
 }
