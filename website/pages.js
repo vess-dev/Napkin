c99fd3fc48a6f050760e626cff97a016d60ab92c
Vess <vess.contact@protonmail.com>
2022-12-02 22:24:36 -0500

    just comments

--- website/pages.js
+++ website/pages.js
@@ -1,3 +1,5 @@
+
+// Insert the WIDE and full Napkin header.
 function insertNapkinHeader() {
 	const elementBody = document.querySelector("body");
 	const elementHeader = document.createElement("div");
@@ -13,66 +15,82 @@ function insertNapkinHeader() {
 	elementBody.append(elementHeader);
 }
 
+// When you need to login to Napkin.
 export function accLogin() {
 	console.log("Login");
 }
 
+// When you are creating an account.
 export function accCreate() {
 	console.log("Create");
 }
 
+// When your account is now pending.
 export function accPending() {
 	console.log("Pending");
 }
 
+// When you are looking at the ALL feed.
 export function myFeed() {
 	insertNapkinHeader();
 }
 
+// When you are looking at the JUST YOU feed.
 export function accFeed() {
 	insertNapkinHeader();
 }
 
+// When you look at your list of friends.
 export function accFriends() {
 	insertNapkinHeader();
 }
 
+// When you look at who you have blocked.
 export function accBlocked() {
 	insertNapkinHeader();
 }
 
+// When you're searching for a friend.
 export function friendSearch() {
 	insertNapkinHeader();
 }
 
+// When you look at your groups.
 export function accGroups() {
 	insertNapkinHeader();
 }
 
+// When you want to create a post.
 export function postCreate() {
 	insertNapkinHeader();
 }
 
+// When you want to edit a post.
 export function postEdit() {
 	insertNapkinHeader();
 }
 
+// When you look at your account settings.
 export function accSettings() {
 	insertNapkinHeader();
 }
 
+// For admins to look at pending accounts.
 export function adminPending() {
 	console.log("Pending");
 }
 
+// For admins to look at disabled accounts.
 export function adminDisabled() {
 	console.log("Disabled");
 }
 
+// For admins to look at active accounts.
 export function adminActive() {
 	console.log("Active");
 }
 
+// For admins to look at blacklisted accounts.
 export function adminBlacklist() {
 	console.log("Blacklist");
 }
