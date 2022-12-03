d54eb1b52d67edd14c58ab6bc15c0939391db6a1
Cathy Sarisky <catsarisky@gmail.com>
2022-12-03 16:34:52 -0500

    query selector problem

--- website/pages.js
+++ website/pages.js
@@ -262,7 +262,7 @@ function postCreateAction() {
     let post_title = document.querySelector('#post_title');
     let post_content = document.querySelector('#post_content');
     let post_image = document.querySelector('#post_image');
-    let selected  = document.querySelectorAll('#group_selector option:selected');
+    let selected  = document.querySelectorAll('#group_selector option:checked');
     let group_ids = Array.from(selected).map(el => el.value);
     let groupList 
     for (let one of group_ids) {groupList += one+','}
