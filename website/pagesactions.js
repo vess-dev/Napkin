db5f44aa5716dc656bc54539767032549fdde5c8
Cathy Sarisky <catsarisky@gmail.com>
2022-12-05 16:35:42 -0500

    undoing a bad attempt to add buttons

--- website/pagesactions.js
+++ website/pagesactions.js
@@ -134,7 +134,7 @@ export function feedFill(postType) {
 			for (let post of postsList) {
 				phelp.insertPost(help.loadImage(post.user_image, true), post.post_title, post.user_handle, 
 				new Date(post.post_timestamp).toLocaleDateString("en-us", {weekday:"long", year:"numeric", month:"short", day:"numeric"}), post.post_content, help.loadImage(post.post_image, true), post.post_likes_score, post.post_comment_count, post.post_id, postType);
-				if (postType) {  phelp.insertPostActions(post.post_id) }
+				//if (postType) {  phelp.insertPostActions(post.post_id) }
 				phelp.insertBigBreak();
 			}
 			return resolve(true);
